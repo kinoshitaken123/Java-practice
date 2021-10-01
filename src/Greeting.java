5807b9de1f3383b37e364481f919517a807e6649
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-01 20:14:00 +0900

    実行

---
+++ src/Greeting.java
@@ -0,0 +1,9 @@
+public class Greeting {
+    public static void main(String[] args){
+        System.out.println("Good morning");
+
+        System.out.println("Good afternoon");
+
+        System.out.println("Good evening");
+    }
+}
