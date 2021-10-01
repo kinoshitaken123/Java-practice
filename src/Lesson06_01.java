f5300ba29ac9571acc27e5e71da53f3624085160
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-01 20:28:41 +0900

    [変数]

---
+++ src/Lesson06_01.java
@@ -0,0 +1,21 @@
+public class Lesson06_01 {
+    public static void main(String[] args) {
+        int var    = 1;
+        int var01  = 2;
+        int var_01 = 3;
+        int var$01 = 4;
+        // int var-01 = 5;
+        // int 01var =6;
+        int VAR = 1;
+        int Var = 2;
+
+        System.out.println(VAR);
+        System.out.println(Var);
+        System.out.println(var);
+        System.out.println(var01);
+        System.out.println(var_01);
+        System.out.println(var$01);
+        // System.out.println(var-01);
+        // System.out.println(01var);
+    }
+}
