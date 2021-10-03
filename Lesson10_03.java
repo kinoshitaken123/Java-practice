f033ec266d3c7621c82b0b16af2a8244952ffe12
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-03 23:25:28 +0900

    [条件分岐]

---
+++ Lesson10_03.java
@@ -0,0 +1,11 @@
+public class Lesson10_03 {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 20) {
+            System.out.println("adult");
+        } else {
+            System.out.println("child");
+        }
+    }
+}
---
+++ src/Lesson10_02.java
@@ -0,0 +1,9 @@
+public class Lesson10_02 {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 20) {
+            System.out.println("adult");
+        }
+    }
+}
